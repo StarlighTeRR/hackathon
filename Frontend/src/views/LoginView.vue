d2d6780af1ae6614f1b667ffa9441e8d727d4641
Сергей Козлов <serzh.kozlov.4@gmail.com>
2024-05-25 14:54:07 +0700

    feat(Frontend): добавлен проект vue js и страницы пользователя

---
+++ Frontend/src/views/LoginView.vue
@@ -0,0 +1,203 @@
+<template>
+    <section className="sign">
+        <div className="container">
+            <div className="sign-content">
+                <div className="sign-form">
+                    <h2 className="form-title">Вход</h2>
+                    <form className="register-form" @submit.prevent="goToRegistration">
+                        <div className="form-group">
+                            <input type="email" v-model="login" placeholder="Логин (email)">
+                        </div>
+                        <div className="form-group">
+                            <input type="password" v-model="password" placeholder="Пароль">
+                        </div>
+                        <div className="form-container">
+                            <div className="form-group form-button">
+                                <input type="submit" @click="Authentificate" className="form-submit" value="Войти">
+                            </div>
+                            <div className="form-button">
+                                <button type="button" className="form-submit"
+                                    @click="$router.push({ name: 'register' })">Регистрация</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            login: '',
+            password: ''
+        };
+    },
+    methods: {
+        Authentificate() {
+        }
+
+    }
+};
+
+</script>
+
+<style scoped>
+.container {
+    width: 20%;
+    min-width: max-content;
+    padding: 20px;
+    background: #fff;
+    border-radius: 10px;
+    display: flex;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+}
+
+/* Основные стили для секции регистрации */
+.sign {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 80vh;
+    background-color: #f2f2f2;
+    /* Цвет фона для секции, вы можете изменить его при необходимости */
+}
+
+.sign-content {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+}
+
+.sign-form {
+    width: 100%;
+}
+
+.form-title {
+    font-size: 24px;
+    color: #333;
+    margin-bottom: 20px;
+    text-align: center;
+}
+
+.form-group,
+.form-button {
+    flex: 1;
+    /* Flex-grow property to make both elements take equal space */
+    margin-right: 10px;
+    /* Add some spacing between the elements */
+}
+
+.form-group:last-child,
+.form-button:last-child {
+    margin-right: 0;
+    /* Remove the margin on the last element */
+}
+
+.form-submit {
+    width: 100%;
+    /* Ensure the input/button takes the full width of the parent */
+}
+
+.form-group,
+.form-button {
+    flex: 1;
+    /* Flex-grow property to make both elements take equal space */
+    margin-right: 10px;
+    /* Add some spacing between the elements */
+    margin-bottom: 15px;
+}
+
+.form-group:last-child,
+.form-button:last-child {
+    margin-right: 0;
+    /* Remove the margin on the last element */
+}
+
+.form-submit {
+    width: 100%;
+    /* Ensure the input/button takes the full width of the parent */
+}
+
+
+.form-group input[type="text"],
+.form-group input[type="email"],
+.form-group input[type="password"] {
+    border-radius: 5px;
+    background: #f9f9f9;
+    width: 100%;
+    height: 40px;
+    display: block;
+    border: none;
+    border-bottom: 1px solid #999;
+    padding: 6px 10px;
+    font-family: 20px Poppins;
+    box-sizing: border-box;
+}
+
+.form-group input[type="date"] {
+    border-radius: 5px;
+    background: #f9f9f9;
+    width: 100%;
+    display: block;
+    border: none;
+    border-bottom: 1px solid #999;
+    padding: 6px 10px;
+    font-family: Poppins;
+    box-sizing: border-box;
+    /* Убираем стрелки для выбора даты */
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+}
+
+.form-group input[type="checkbox"] {
+    margin-right: 10px;
+}
+
+.form-container {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+}
+
+.label-agree-term {
+    font-size: 14px;
+    color: #333;
+}
+
+.label-agree-term a {
+    color: #3C388D;
+    text-decoration: none;
+}
+
+.label-agree-term a:hover {
+    text-decoration: underline;
+}
+
+
+.form-button {
+    display: flex;
+    justify-content: center;
+}
+
+.form-submit {
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    background: #3C388D;
+    color: #fff;
+    font-size: 16px;
+    cursor: pointer;
+    transition: background 0.3s;
+}
+
+.form-submit:hover {
+    background: #2e2a72;
+}
+</style>
---
+++ Frontend/src/views/QuestionnaireView.vue
@@ -0,0 +1,200 @@
+<template>
+    <div class="main-container">
+        <div class="form-group form-container">
+            <h1>Моя анкета</h1>
+            <div>
+                <p>Ваша заявка успешно отправлена. Мы свяжемся с вами в ближайшее время по указанным вами контактам.</p>
+                <button type="button" @click="editApplication">Подать заявку заново</button>
+            </div>
+        </div>
+        <form class="form-container" @submit.prevent="submitApplication">
+            <div class="form-group">
+                <label for="lastName">Фамилия</label>
+                <input type="text" id="lastName" v-model="form.lastName" disabled />
+            </div>
+            <div class="form-group">
+                <label for="firstName">Имя</label>
+                <input type="text" id="firstName" v-model="form.firstName" disabled />
+            </div>
+            <div class="form-group">
+                <label for="middleName">Отчество</label>
+                <input type="text" id="middleName" v-model="form.middleName" disabled />
+            </div>
+            <div className="form-group">
+                <label for="birthDate">Дата рождения</label>
+                <input type="date" id="birthDate" v-model="form.birthDate" disabled />
+            </div>
+            <div class="form-group">
+                <label for="gender">Пол</label>
+                <select id="gender" v-model="form.gender" required>
+                    <option value="male">Мужской</option>
+                    <option value="female">Женский</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="graduationYear">Год окончания обучения в школе</label>
+                <input type="number" id="graduationYear" v-model="form.graduationYear" required />
+            </div>
+            <div class="form-group">
+                <label for="faculty">Факультет</label>
+                <select id="faculty" v-model="form.faculty" required>
+                    <option value="faculty1">Факультет 1</option>
+                    <option value="faculty2">Факультет 2</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="email">Электронная почта для связи</label>
+                <input type="email" id="email" v-model="form.email" required />
+            </div>
+            <div class="form-group">
+                <label for="phone">Мобильный телефон</label>
+                <input type="tel" id="phone" v-model="form.phone" required />
+            </div>
+            <button type="submit">Отправить</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            form: {
+                lastName: '',
+                firstName: '',
+                middleName: '',
+                birthDate: '',
+                gender: '',
+                graduationYear: '',
+                faculty: '',
+                email: '',
+                phone: '',
+            },
+            submitted: false,
+        };
+    },
+    created() {
+        // Инициализация формы с данными пользователя
+        this.form.lastName = 'Иванов';
+        this.form.firstName = 'Иван';
+        this.form.middleName = 'Иванович';
+        this.form.birthDate = '2000-01-01';
+        this.form.email = 'ivanov@example.com';
+    },
+    methods: {
+        submitApplication() {
+
+        },
+        editApplication() {
+
+        },
+    },
+};
+</script>
+
+<style scoped>
+.error {
+    color: red;
+}
+
+
+.main-container {
+    background-color: white;
+    color: #3C388D;
+    padding: 20px;
+    font-family: Arial, sans-serif;
+}
+
+h1 {
+    font-family: "NekstRegular", "Verdana", sans-serif;
+    font-size: 2em;
+    text-align: center;
+    margin-bottom: 0.5em;
+}
+
+.info-text {
+    font-family: "NekstRegular", "Verdana", sans-serif;
+    color: #5F5F5F;
+    margin-bottom: 2em;
+}
+
+.form-container {
+    width: 50%;
+    margin: 0 auto;
+}
+
+h2 {
+    font-size: 1.5em;
+    margin-bottom: 1em;
+    text-align: center;
+}
+
+.form-group {
+    margin-bottom: 1em;
+}
+
+label {
+    display: block;
+    font-weight: bold;
+    margin-bottom: 0.5em;
+}
+
+input,
+textarea {
+    width: 100%;
+    padding: 0.5em;
+    border: 1px solid #CCC;
+    border-radius: 4px;
+    margin-bottom: 0.5em;
+    padding: 0.5em;
+    height: 2em;
+}
+
+select {
+    width: 100px;
+    box-sizing: border-box;
+    padding: 0.5em;
+    border: 1px solid #CCC;
+    border-radius: 4px;
+    margin-bottom: 0.5em;
+    height: auto;
+}
+
+textarea {
+    height: 8em;
+}
+
+button {
+    padding: 0.5em;
+    background-color: #007BFF;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    display: block;
+    width: 100%;
+    font-size: 1em;
+}
+
+button:hover {
+    background-color: #0056b3;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    margin-bottom: 1em;
+}
+
+li button {
+    margin-left: 1em;
+    background-color: #28a745;
+}
+
+li button:hover {
+    background-color: #218838;
+}
+</style>
