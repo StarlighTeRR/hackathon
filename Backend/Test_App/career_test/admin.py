5990a12439e60ebdaf86cbecb6181e2690f4e7c0
Сергей Козлов <serzh.kozlov.4@gmail.com>
2024-05-25 18:33:42 +0700

    feat(Backend): добавлен проект для backend'а

--- Backend/Test_App/career_test/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Question, Answer, Result, Choice
-
-admin.site.register(Question)
-admin.site.register(Answer)
-admin.site.register(Result)
-admin.site.register(Choice)
