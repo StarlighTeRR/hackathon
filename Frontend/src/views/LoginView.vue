94d624b926190b008e417e4a4caf2f0bcc530e19
Сергей Козлов <serzh.kozlov.4@gmail.com>
2024-05-26 11:04:43 +0700

    feat(Frontend): добавлен запрос на сервер

--- Frontend/src/views/LoginView.vue
+++ Frontend/src/views/LoginView.vue
@@ -28,6 +28,7 @@
   </template>
   
   <script>
+  import axios from 'axios';
   export default {
     data() {
       return {
@@ -37,12 +38,27 @@
       };
     },
     methods: {
-      Authentificate() {
+      async Authentificate() {
         if (this.isPasswordValid(this.password)) {
             this.errorMessage = ''
-          alert('Вход выполнен');
-        } else {
-          this.errorMessage = 'Пароль может сожержать только латинские буквы и цифры, длина не менее 8 символов';
+          
+            try {
+                const response = await axios.post('/login', {
+                    login: this.login,
+                    password: this.password
+                });
+                alert(response.data.message);
+            } 
+            catch (error) {
+                if (error.response) {
+                  this.errorMessage = error.response.data.message;
+                } else {
+                    alert('Ошибка связи с сервером.');
+                }
+            }
+        } 
+        else {
+          this.errorMessage = 'Пароль латинские буквы и цифры, длина не менее 8 символов';
         }
       },
       isPasswordValid(password) {
