7745c73de17d6a35061b0cb4efcecc8e3e1e9f1d
StarlighTeRR <zabbarove22@gmail.com>
2024-05-26 18:52:46 +0700

    app.py facultyselection

--- Backend/app.py
+++ Backend/app.py
@@ -7,7 +7,27 @@ from sqlalchemy.ext.declarative import declarative_base
 from flask_jwt_extended import JWTManager, jwt_required
 from flask_cors import CORS
 from config import Config
+import joblib 
+from sklearn.preprocessing import LabelEncoder
 
+
+direction_names = {
+    2: "Информатика и вычислительная техника",
+    3: "Программная инженерия",
+    4: "Информационная безопасность",
+    5: "Биология",
+    6: "Математика и математическое моделирование"
+}
+
+def predict_direction(model, label_encoder, subjects, direction_names):
+    text_representation = ' '.join(map(str, subjects))
+    prediction_encoded = model.predict([text_representation])
+    prediction_decoded = label_encoder.inverse_transform(prediction_encoded)
+    predicted_direction = direction_names[prediction_decoded[0]]
+    return predicted_direction
+
+model = joblib.load('model.joblib')
+label_encoder = LabelEncoder()
 app = Flask(__name__)
 CORS(app, resources={r'/*': {'origins': '*'}})
 client = app.test_client()
@@ -23,7 +43,7 @@ Base.query = session.query_property()
 jwt = JWTManager(app)
 
 from models import *
-
+models = joblib.load('model.joblib')
 Base.metadata.create_all(bind=engine)
 
 @app.route('/api/register', methods=['POST'])
@@ -46,16 +66,25 @@ def login():
 def facultyselection():
     data = request.get_json()
 
+    # Получение строки с названиями предметов из данных пользователя
+    subjects_str = data['subjects']
 
-    # Возьми значение из data и составь результат result
-    # Записб в бд результата и дданных пользователя
+    # Преобразование строки с предметами в список, разделяя по запятой
+    subjects_list = subjects_str.split(',')
 
+    # Сопоставление каждого предмета с вашим словарем направлений
+    favorite_subjects = [1 if subject.strip() in data['subjects'] else 0 for subject in direction_names.values()]
+
+    # Прогнозирование направления с использованием вашей модели
+    predicted_direction = predict_direction(model, label_encoder, favorite_subjects, direction_names)
+
+    # Запись результата и данных пользователя в базу данных 
     faculty = FacultyForm(**data)
+    faculty.result = predicted_direction
     session.add(faculty)
     session.commit()
 
-    result = "Результат"
-    return jsonify({'facultyresult': "Результат"})
+    return jsonify({'Результат': predicted_direction})
 
 if __name__ == '__main__':
     app.run(debug=True)
